3cf7951d6a69cae98674d5a003056465d1a3ace4
kojimadev <kojimadev@gmail.com>
2021-06-19 15:21:16 +0900

    Appを FunctionからClassに変更

--- src/App.js
+++ src/App.js
@@ -1,86 +1,97 @@
 import './App.css';
 import React from 'react';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  //const [状態変数, 状態を変更するための関数] = useState(状態の初期値);
-  const [val, setVal] = React.useState('choice1');
+    this.state = {
+      val: 'choice1'
+    };
+  }
+
+  // React hooksを用いてuseStateを利用する例
+  // const [状態変数, 状態を変更するための関数] = useState(状態の初期値);
+  // const [val, setVal] = React.useState('choice1');
+  // https://qiita.com/seira/items/f063e262b1d57d7e78b4
 
   // ラジオボタンの変更時ハンドラ
-  const handleChange = e => {
-    setVal(e.target.value);
-    console.log('handleChange:'+ e.target.value);
+  handleChange(value) {
+    this.setState({
+      val:value,
+    });
+    console.log('handleChange:'+ value);
+  }
+
+  // 次へボタン押下時ハンドラ
+  handleNextButtonClick(value) {
+    console.log('Click happened:' + value);
     //alert("alertの確認");
   }
 
-  /*
-  const handleNextButtonClick() {
-    console.log('Click happened');
+  render() {
+
+    return (
+      <div className="App">
+        <p>
+          問題<br/>
+          問題文1行目<br/>
+          問題文2行目<br/>
+          問題文3行目<br/>
+          問題文4行目<br/>
+        </p>
+
+        {/* 選択肢の表示 */}
+
+        <label>
+          <input
+            type="radio"
+            value="choice1"
+            onChange={() => this.handleChange('choice1')}
+            checked={this.state.val === 'choice1'}
+          />
+          選択肢1つ目
+        </label>
+        <br/>
+        <label>
+          <input
+            type="radio"
+            value="choice2"
+            onChange={() => this.handleChange('choice2')}
+            checked={this.state.val === 'choice2'}
+          />
+          選択肢2つ目
+        </label>
+        <br/>
+        <label>
+          <input
+            type="radio"
+            value="choice3"
+            onChange={() => this.handleChange('choice3')}
+            checked={this.state.val === 'choice3'}
+          />
+          選択肢3つ目
+        </label>
+        <br/>
+        <label>
+          <input
+            type="radio"
+            value="choice4"
+            onChange={() => this.handleChange('choice4')}
+            checked={this.state.val === 'choice4'}
+          />
+          選択肢4つ目
+        </label>
+        <br/>
+
+        <button 
+          onClick={() => this.handleNextButtonClick(this.state.val)}>
+          次へ
+        </button>
+
+      </div>
+    );
   }
-  */
-  function handleNextButtonClick() {
-    console.log('Click happened');
-  }
-
-  return (
-    <div className="App">
-      <p>
-        問題<br/>
-        問題文1行目<br/>
-        問題文2行目<br/>
-        問題文3行目<br/>
-        問題文4行目<br/>
-      </p>
-
-      {/* 選択肢の表示 */}
-
-      <label>
-        <input
-          type="radio"
-          value="choice1"
-          onChange={handleChange}
-          checked={val === 'choice1'}
-        />
-        選択肢1つ目
-      </label>
-      <br/>
-      <label>
-        <input
-          type="radio"
-          value="choice2"
-          onChange={handleChange}
-          checked={val === 'choice2'}
-        />
-        選択肢2つ目
-      </label>
-      <br/>
-      <label>
-        <input
-          type="radio"
-          value="choice3"
-          onChange={handleChange}
-          checked={val === 'choice3'}
-        />
-        選択肢3つ目
-      </label>
-      <br/>
-      <label>
-        <input
-          type="radio"
-          value="choice4"
-          onChange={handleChange}
-          checked={val === 'choice4'}
-        />
-        選択肢4つ目
-      </label>
-      <br/>
-
-      <button onclick={handleNextButtonClick}>
-        次へ
-      </button>
-
-    </div>
-  );
 }
 
 export default App;
