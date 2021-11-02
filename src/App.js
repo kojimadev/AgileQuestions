d5550788052e2eb40107c4deaf230c1c89e08036
kojimadev <kojimadev@gmail.com>
2021-11-02 19:29:17 +0900

    以下を参考に、子コンポーネントから親コンポーネントに値を反映するように修正
    https://www.to-r.net/media/react-tutorial10/

--- src/App.js
+++ src/App.js
@@ -3,56 +3,78 @@ import React from 'react';
 import QuestionArea from './QuestionArea';
 
 class App extends React.Component {
+  // コンストラクタ
   constructor(props) {
     super(props);
 
     // 設問の定義
-    this.state = [
-      {
-        problemRound: 1,
-        problemStatement: 'ペアプログラミングもしくはモブプログラミングについて、\nそれが有効なタスクの特性をチームメンバーが理解した上で、活用できている。',
-      },
-      {
-        problemRound: 2,
-        problemStatement: 'レビューで指摘した結果、想定以上に多くの指摘を検出して手戻りが発生した場合に、\nそれがレビューイのみの責任でなく、レビューアにも責任があるという考え方がチームに浸透している。',
-      },
-      {
-        problemRound: 3,
-        problemStatement: 'プロジェクトの業務時間の一定比率(比率はプロジェクトの方針による)で、\n各自が主体的にスキルアップや改善を行う活動を毎月実施できている。',
-      },
-      {
-        problemRound: 4,
-        problemStatement: '新しいツールや技術やプラクティスを試してみようとする文化があり、\n毎月なんらかの試行が行われている。',
-      },
-      {
-        problemRound: 5,
-        problemStatement: 'チームとしての方針を決める打ち合わせにおいて、チーム全員が積極的な姿勢で発言できている(配属して1年未満の新人は除く)。',
-      },
-      {
-        problemRound: 6,
-        problemStatement: 'チーム全員が学んだ知見をブログや分報やSNSなどに継続的にアウトプットしている(少なくとも毎月1回以上)。',
-      },
-      {
-        problemRound: 7,
-        problemStatement: 'リモートワークにおいて、チーム全員が気軽にビデオ通話に誘える関係性があり、分報を用いて誰が今何をしているか透明性がある。',
-      },
-    ];
+    this.state = {
+      questionStates : [
+        {
+          problemRound: 1,
+          problemStatement: 'ペアプログラミングもしくはモブプログラミングについて、\nそれが有効なタスクの特性をチームメンバーが理解した上で、活用できている。',
+        },
+        {
+          problemRound: 2,
+          problemStatement: 'レビューで指摘した結果、想定以上に多くの指摘を検出して手戻りが発生した場合に、\nそれがレビューイのみの責任でなく、レビューアにも責任があるという考え方がチームに浸透している。',
+        },
+        {
+          problemRound: 3,
+          problemStatement: 'プロジェクトの業務時間の一定比率(比率はプロジェクトの方針による)で、\n各自が主体的にスキルアップや改善を行う活動を毎月実施できている。',
+        },
+        {
+          problemRound: 4,
+          problemStatement: '新しいツールや技術やプラクティスを試してみようとする文化があり、\n毎月なんらかの試行が行われている。',
+        },
+        {
+          problemRound: 5,
+          problemStatement: 'チームとしての方針を決める打ち合わせにおいて、チーム全員が積極的な姿勢で発言できている(配属して1年未満の新人は除く)。',
+        },
+        {
+          problemRound: 6,
+          problemStatement: 'チーム全員が学んだ知見をブログや分報やSNSなどに継続的にアウトプットしている(少なくとも毎月1回以上)。',
+        },
+        {
+          problemRound: 7,
+          problemStatement: 'リモートワークにおいて、チーム全員が気軽にビデオ通話に誘える関係性があり、分報を用いて誰が今何をしているか透明性がある。',
+        },
+      ]
+    };
   }
 
+  // 各設問コンポーネント(QuestionArea)から、このコンポーネントのステートに値を反映させるためのメソッド
+  updateState(problemRound, value){
+    // 対象範囲のステートをコピー
+    const questionStates_copy = this.state.questionStates.slice();
+    // 指定した設問の選択状態を変更
+    questionStates_copy[problemRound - 1].val = value;
+    // ステートに反映
+    this.setState({
+      questionStatements : questionStates_copy
+    });
+  }
+
+  // 描画する
   render() {
+
+    // 設問の定義
     var questionStatements = [];
-    for(var index in this.state){
-      questionStatements.push(<QuestionArea problemRound={this.state[index].problemRound} problemStatement={this.state[index].problemStatement} />);
+    for(var index in this.state.questionStates){
+      questionStatements.push(<QuestionArea 
+        problemRound={this.state.questionStates[index].problemRound} 
+        problemStatement={this.state.questionStates[index].problemStatement} 
+        updateState={this.updateState.bind(this)}
+        />);
     }
 
+    // 設問と診断結果ボタンの表示
     return(
       <div className="App">
         <div className="questions">
           {questionStatements}
         </div>
         <br/>
-        <button 
-        onClick={() => this.handleNextButtonClick(this.state.val)}>
+        <button onClick={() => this.handleNextButtonClick()}>
         診断結果へ
         </button>
         <br/>
@@ -62,10 +84,12 @@ class App extends React.Component {
   }
 
   // 診断結果ボタン押下時ハンドラ
-  handleNextButtonClick(value) {
-    console.log('Click happened:' + value);    
+  handleNextButtonClick() {
+    console.log('NextButton Clicked');
+    for (let index = 0; index < this.state.questionStates.length; index++) {
+      console.log(index + ':' + this.state.questionStates[index].val);
+    }
   }
-
 }
 
 export default App;
--- src/QuestionArea.js
+++ src/QuestionArea.js
@@ -3,27 +3,32 @@ import React from 'react';
 // 設問エリアを表示するクラス
 export default class QuestionArea extends React.Component {
 
+    // コンストラクタ
     constructor(props) {
         super(props);
     
+        // 選択肢の初期値設定
         this.state = {
-          problemRound: props.problemRound,
-          val: '',
-          problemStatement: props.problemStatement,
-          choiceState1: 'はい',
-          choiceState2: 'たぶん はい',
-          choiceState3: 'たぶん いいえ',
-          choiceState4: 'いいえ',
+            problemRound:props.problemRound,
+            val: '',
         };
     }
 
+    // コンポーネントを描画する
     render() {
+        // 選択肢の表示文字列定義
+        const choiceState1 = "はい";
+        const choiceState2 = "たぶん はい";
+        const choiceState3 = "たぶん いいえ";
+        const choiceState4 = "いいえ";
+
+        // 設問と選択肢を表示する
         return (
         <div className="QuestionArea" style={{whiteSpace: 'pre-line'}}>
 
             {/* 設問の表示 */}
             <p>
-            {this.state.problemStatement}
+            {this.props.problemStatement}
             </p>
 
             {/* 選択肢の表示 */}
@@ -35,7 +40,7 @@ export default class QuestionArea extends React.Component {
                 onChange={() => this.handleChange('choice1')}
                 checked={this.state.val === 'choice1'}
             />
-            {this.state.choiceState1}
+            {choiceState1}
             </label>
 
             <label>
@@ -45,7 +50,7 @@ export default class QuestionArea extends React.Component {
                 onChange={() => this.handleChange('choice2')}
                 checked={this.state.val === 'choice2'}
             />
-            {this.state.choiceState2}
+            {choiceState2}
             </label>
 
             <label>
@@ -55,7 +60,7 @@ export default class QuestionArea extends React.Component {
                 onChange={() => this.handleChange('choice3')}
                 checked={this.state.val === 'choice3'}
             />
-            {this.state.choiceState3}
+            {choiceState3}
             </label>
 
             <label>
@@ -65,21 +70,24 @@ export default class QuestionArea extends React.Component {
                 onChange={() => this.handleChange('choice4')}
                 checked={this.state.val === 'choice4'}
             />
-            {this.state.choiceState4}
-        </label>
-            <br/>
+            {choiceState4}
+            </label>
 
         </div>
         );
     }
 
-
     // ラジオボタンの変更時ハンドラ
     handleChange(value) {
-        this.setState({  
-            val:value,
-    });
-    console.log('handleChange:'+ value);
-  }
-
+        // stateに選択状態を反映
+        this.setState({
+            val:value
+          }, () => {
+            // setStateは非同期で行われるため、stateの値を用いた後続処理は、反映後のコールバックで定義
+            // 以下の記事を参考に、親コンポーネントから引き継いだupdateState()を実行することで親コンポーネントのstateを変更
+            // https://www.to-r.net/media/react-tutorial10/
+            this.props.updateState(this.props.problemRound, this.state.val);
+            console.log('handleChange:'+ value);
+          });
+    }
 }
