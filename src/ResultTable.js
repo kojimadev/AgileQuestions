3b6db9836ecb4b9d81911091f744b7d89fc5802c
kojimadev <kojimadev@gmail.com>
2022-03-29 15:07:29 +0900

    設問を改善

--- src/ResultTable.js
+++ src/ResultTable.js
@@ -61,10 +61,11 @@ export default class ResultTable extends React.Component {
 
                 <p>
                     よろしければ、以下の記事を参考に<br/>
-                    得点アップにつながるヒントを見つけてもらえれば幸いです。<br/>
+                    楽しいチーム開発をするためのヒントを見つけてもらえれば幸いです。<br/>
                     <a href="https://qiita.com/kojimadev/items/4b28f801863cf4e8f0da" target="_blank" rel="noopener noreferrer">1年以上かけて生産性倍増＋成長し続けるチームになった施策を全部公開</a><br/>
-                    <a href="https://qiita.com/kojimadev/items/e12784e6764f1b60e73c" target="_blank" rel="noopener noreferrer">レビューで大量の指摘をして大きな手戻りを発生させた原因はレビューアの私にあった</a><br/>
                     <a href="https://qiita.com/kojimadev/items/c211207ede652c2abeb0" target="_blank" rel="noopener noreferrer">疲労感と孤独感いっぱいのリモートワークからの脱却</a><br/>
+                    <a href="https://qiita.com/kojimadev/items/e12784e6764f1b60e73c" target="_blank" rel="noopener noreferrer">レビューで大量の指摘をして大きな手戻りを発生させた原因はレビューアの私にあった</a><br/>
+                    <a href="https://www.docswell.com/s/kojimadev/ZWRL1K-2022-03-16-200900" target="_blank" rel="noopener noreferrer">主体性を発揮しやすい環境を作る事例</a><br/>
                 </p>
 			</table>
 		);
